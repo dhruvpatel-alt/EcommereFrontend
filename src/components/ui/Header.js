1a9ef34cf2a8581eccae27d5f48dbe6e1367f3c9
dhruv patel <dhruv160916@gmail.com>
2023-03-15 20:06:24 +0530

    changed

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -40,7 +40,8 @@ const handleCart=()=>{
 }
 const activeIndex=()=>{
   for (let i = 0; i < 4; i++) {
-    if(typeof window!==null){
+      if (typeof window !== 'undefined') {
+
       if(`${routes[i].Name.toLowerCase()}`=== window.location.pathname.split('/')[1]||routes[i].link===window.location.pathname){
       return i;
     }}else return 0;
