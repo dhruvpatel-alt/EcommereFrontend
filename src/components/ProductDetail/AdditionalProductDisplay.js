cd375c849a24ac1ed568a741112808db27569355
dhruv patel <dhruv160916@gmail.com>
2023-03-16 15:19:16 +0530

    final commit

--- src/components/ProductDetail/AdditionalProductDisplay.js
+++ src/components/ProductDetail/AdditionalProductDisplay.js
@@ -5,7 +5,7 @@ import './ProductDisplay.css'
 function AdditionalProductDisplay({product}) {
   var splitName=product.product_2.Name.toLowerCase()
   var variant=product.product_2.variants[product.Index]
-  var style;    
+    var style;
   if(!variant.style){style=null}else{style=variant.style||undefined}
   var color=variant.Color_label
   return (
